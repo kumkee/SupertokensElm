aabad8ff381df5551853f4c0635fa167cb9a2a7e
Jinjun Liang <kumkee@users.noreply.github.com>
2023-03-11 15:37:13 +1300

    added Google login

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
-from secrect_strs import SUPERTOKENS_API
-from supertokens_python import init, InputAppInfo, SupertokensConfig
+"""Backend linking to SuperTokens core."""
+from fastapi import FastAPI
+from secrect_strs import GOOGLE_0AUTH_CLIENT_SECRET, SUPERTOKENS_API
+from starlette.middleware.cors import CORSMiddleware
+from supertokens_python import (
+    get_all_cors_headers,
+    init,
+    InputAppInfo,
+    SupertokensConfig,
+)
+from supertokens_python.framework.fastapi import get_middleware
 from supertokens_python.recipe import session, thirdpartyemailpassword
+from supertokens_python.recipe.thirdpartyemailpassword import Google
+
 
 init(
     app_info=InputAppInfo(
@@ -25,3 +36,36 @@ init(
     ],
     mode='asgi',  # use wsgi if you are running using gunicorn
 )
+
+
+# Inside init
+thirdpartyemailpassword.init(
+    providers=[
+        # We have provided you with development keys which you can use for testing.
+        # IMPORTANT: Please replace them with your own OAuth keys for production use.
+        Google(
+            client_id='327020114390-ggc01io6rm7o7h5k772kgon9nd3mch8k'
+            + '.apps.googleusercontent.com',
+            client_secret=GOOGLE_0AUTH_CLIENT_SECRET
+            # ), Facebook(
+            #     client_id='FACEBOOK_CLIENT_ID',
+            #     client_secret='FACEBOOK_CLIENT_SECRET'
+        ),
+    ]
+)
+
+
+app = FastAPI()
+app.add_middleware(get_middleware())
+
+# TODO: Add APIs
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost'],
+    allow_credentials=True,
+    allow_methods=['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH'],
+    allow_headers=['Content-Type'] + get_all_cors_headers(),
+)
+
+# TODO: start server
